961d50230e898d59a5eacdf978cbaf3cae02fe0e
Minh Nguyen <10432272-tmnguyen1403@users.noreply.gitlab.com>
2023-03-16 00:36:44 -0400

    updated constructor exception example

--- FundamentalC++20/constructor_exception.cpp
+++ FundamentalC++20/constructor_exception.cpp
@@ -4,16 +4,28 @@
 class Failed
 {
     public:
-    Failed(){
-        std::cout << "Failed class\n";
+    explicit Failed(){
+        std::cout << "Failed constructor class\n";
         throw std::runtime_error("Failed constructor failed");
     }
+    explicit Failed(int value){
+        std::cout << "Failed constructor class\n";
+        throw std::runtime_error("Failed constructor failed");
+    }
+    private:
+    int i{};
 };
 
 class HandleFailed
 {
     public:
-    HandleFailed(Failed f) try : f_{f} {
+    explicit HandleFailed(Failed f) try : f_{f} {
+        std::cout << "HandleFailed constructor class\n";
+
+    } catch (const std::runtime_error& e) {
+        std::cout << "exception: " << e.what() << std::endl;
+    }
+    explicit HandleFailed(int value) try : f_{value} {
         std::cout << "HandleFailed constructor class\n";
 
     } catch (const std::runtime_error& e) {
@@ -27,6 +39,14 @@ int main()
 {
     try{
      HandleFailed h{Failed{}};
+
+    } catch(const std::runtime_error& e) {
+        std::cout << "caught in main: " << e.what() << std::endl;
+    }
+     
+     std::cout << "\nThe below execution generates exception aught in HandleFailed constructor first, then rethrow\n";
+     try{
+     HandleFailed h{123};
 
     } catch(const std::runtime_error& e) {
         std::cout << "caught in main: " << e.what() << std::endl;
