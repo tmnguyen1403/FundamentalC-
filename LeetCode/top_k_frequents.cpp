9d91c14a03d304c13ea22e03f3538ba03aba04d1
Minh Nguyen <10432272-tmnguyen1403@users.noreply.gitlab.com>
2024-01-05 23:55:57 -0500

    solved top_k_frequents using priority queue

---
+++ LeetCode/top_k_frequents.cpp
@@ -0,0 +1,27 @@
+#include <queue>
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int,int> freq;
+        for (const auto& num : nums) {
+            if (freq.find(num) == freq.end())
+                freq[num] = 0;
+            freq[num] += 1;
+        }
+        auto compare = [&](int a, int b) {
+            return freq[a] < freq[b];
+        };
+        std::priority_queue<int,std::vector<int>,decltype(compare)> q(compare, std::vector<int>{});
+        for (const auto& [key, val]: freq) {
+            q.push(key);
+        }
+        std::vector<int> result;
+        for(int i = 0; i < k; ++i) {
+            result.push_back(q.top());
+            q.pop();
+        }
+        return result;
+    }
+};
