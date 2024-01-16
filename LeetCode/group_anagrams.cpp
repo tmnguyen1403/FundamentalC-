7d392397732d4a8a00f48a6f13e6f9300e382bc8
Minh Nguyen <10432272-tmnguyen1403@users.noreply.gitlab.com>
2024-01-15 23:07:12 -0500

    implemented using frequency count

--- LeetCode/group_anagrams.cpp
+++ LeetCode/group_anagrams.cpp
@@ -19,4 +19,36 @@ public:
         }
         return result;
     }
+    /*
+    Use frequency count
+    */
+    auto getKey(std::string_view s) {
+        std::array<uint32_t,26> counts{};
+        for (auto c : s) {
+            counts[c - 'a'] += 1;
+        }
+        std::string result{""};
+        for (auto value : counts) {
+            result += std::to_string(value) + "#";
+        }
+        return result;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //map the hash value to the index in the result vector
+        std::unordered_map<std::string, int> store_result;
+        std::vector<std::vector<std::string>> result;
+        for (const auto& s: strs) {
+            //use frequency count approach
+            auto key = getKey(s);
+            if (store_result.find(key) == store_result.end()) {
+                std::vector<std::string> tmp{s};
+                result.push_back(tmp);
+                store_result[key] = result.size() - 1;
+            } else {
+                result[store_result[key]].push_back(s);
+            }
+        }
+        return result;
+    }
 };
